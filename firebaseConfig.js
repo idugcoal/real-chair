dfc48f9eeae5ce8a2c4a0a62387224d7729cc8c2
Doug Cole <idugcoal@gmail.com>
2023-04-01 12:58:05 -0700

    read firebase values from config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,17 +1,20 @@
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from "firebase/app"
 import Config from "react-native-config"
+
+import { getAuth } from "firebase/auth"
+import { getDatabase } from "firebase/database"
 
 // Initialize Firebase
 const firebaseConfig = {
   apiKey: Config.FIREBASE_API_KEY,
-  authDomain: 'real-chair.firebaseapp.com',
-  databaseURL: 'https://real-chair.firebaseio.com',
-  projectId: 'real-chair',
-  storageBucket: 'real-chair.appspot.com',
-  messagingSenderId: 'sender-id',
-  appId: 'real-chair',
-  measurementId: 'G-measurement-id',
-};
+  authDomain: Config.FIREBASE_AUTH_DOMAIN,
+  databaseURL: Config.FIREBASE_DATABASE_URL,
+  projectId: Config.FIREBASE_PROJECT_ID,
+  storageBucket: Config.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Config.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Config.FIREBASE_APP_ID,
+}
 
-const app = initializeApp(firebaseConfig);
-
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const database = getDatabase(app)
