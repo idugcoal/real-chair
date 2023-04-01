5bb6047d6d489efcf974f353db3084fe16363803
Doug Cole <idugcoal@gmail.com>
2023-04-01 16:30:25 -0700

    adds app_strings file

---
+++ assets/app_strings.js
@@ -0,0 +1,12 @@
+const home = {
+    title: "RealChair",
+    subtitle: "Tracking wheelchairs with the power of Ben's balls.",
+    buttonTitle: "Tap to start",
+}
+
+const wheelchairList = {
+    title: "Wheelchairs",
+    buttonTitle: "Go back",
+}
+
+export { home, wheelchairList }
