0bcf7d163972394d408acbd079f027ba544a99ea
Doug Cole <idugcoal@gmail.com>
2023-04-01 16:30:37 -0700

    consumes app_strings file

--- app/index.js
+++ app/index.js
@@ -4,60 +4,58 @@ import { getAuth } from "firebase/auth"
 import { getDatabase, ref, set } from "firebase/database"
 import { useRouter } from "expo-router"
 import { firebaseConfig } from "../firebase"
+import { home } from "../assets/app_strings"
 
 const Page = () => {
-  const router = useRouter()
-  const app = initializeApp(firebaseConfig)
+    const router = useRouter()
+    const app = initializeApp(firebaseConfig)
 
-  const auth = getAuth(app)
-  const database = getDatabase(app)
+    const auth = getAuth(app)
+    const database = getDatabase(app)
 
-  const writeUserData = () => {
-    const db = getDatabase()
-    set(ref(db, "Users/" + "12345"), {
-      username: "test",
-      email: "test@example.com",
-    })
-  }
+    const writeUserData = () => {
+        const db = getDatabase()
+        set(ref(db, "Users/" + "12345"), {
+            username: "test",
+            email: "test@example.com",
+        })
+    }
 
-  onButtonPress = () => {
-    router.push("./wheelchair_list")
-  }
+    onButtonPress = () => {
+        router.push("./wheelchair_list")
+    }
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>RealChair</Text>
-        <Text style={styles.subtitle}>
-          Tracking wheelchairs with the power of Ben's balls.
-        </Text>
-        <Button title="Tap to start" onPress={onButtonPress} />
-        <Button title="Add a fact about Ben" onPress={() => writeUserData()} />
-      </View>
-    </View>
-  )
+    return (
+        <View style={styles.container}>
+            <View style={styles.main}>
+                <Text style={styles.title}>{home.title}</Text>
+                <Text style={styles.subtitle}>{home.subtitle}</Text>
+                <Button title={home.buttonTitle} onPress={onButtonPress} />
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        padding: 24,
+    },
+    main: {
+        flex: 1,
+        justifyContent: "center",
+        maxWidth: 960,
+        marginHorizontal: "auto",
+    },
+    title: {
+        fontSize: 64,
+        fontWeight: "bold",
+    },
+    subtitle: {
+        fontSize: 36,
+        color: "#38434D",
+    },
 })
 
 export default Page
--- app/wheelchair_list.js
+++ app/wheelchair_list.js
@@ -2,37 +2,37 @@ import { Button, StyleSheet, Text, View } from "react-native"
 import { useRouter } from "expo-router"
 
 const WheelchairList = () => {
-  const router = useRouter()
+    const router = useRouter()
 
-  onButtonPress = () => {
-    router.back()
-  }
+    onButtonPress = () => {
+        router.back()
+    }
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Wheelchairs</Text>
-        <Button onPress={onButtonPress} title="Go back" />
-      </View>
-    </View>
-  )
+    return (
+        <View style={styles.container}>
+            <View style={styles.main}>
+                <Text style={styles.title}>Wheelchairs</Text>
+                <Button onPress={onButtonPress} title="Go back" />
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: "bold",
-  },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        padding: 24,
+    },
+    main: {
+        flex: 1,
+        maxWidth: 960,
+        marginHorizontal: "auto",
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: "bold",
+    },
 })
 
 export default WheelchairList
