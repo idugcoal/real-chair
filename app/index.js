03885eb738868869baa12598dafeab03a5cf0cd3
Doug Cole <idugcoal@gmail.com>
2023-04-01 14:54:50 -0700

    writes to firebase

--- app/index.js
+++ app/index.js
@@ -1,12 +1,29 @@
 import { Button, StyleSheet, Text, View } from "react-native"
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getDatabase, ref, set } from "firebase/database"
 import { useRouter } from "expo-router"
+import { firebaseConfig } from "../firebase"
 
 const Page = () => {
   const router = useRouter()
+  const app = initializeApp(firebaseConfig)
+
+  const auth = getAuth(app)
+  const database = getDatabase(app)
+
+  const writeUserData = () => {
+    const db = getDatabase()
+    set(ref(db, "Users/" + "12345"), {
+      username: "test",
+      email: "test@example.com",
+    })
+  }
 
   onButtonPress = () => {
     router.push("./wheelchair_list")
   }
+
   return (
     <View style={styles.container}>
       <View style={styles.main}>
@@ -15,6 +32,7 @@ const Page = () => {
           Tracking wheelchairs with the power of Ben's balls.
         </Text>
         <Button title="Tap to start" onPress={onButtonPress} />
+        <Button title="Add a fact about Ben" onPress={() => writeUserData()} />
       </View>
     </View>
   )
